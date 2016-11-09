068e958acf69963fe62ae326e3398b751c346514
ParashRahman <parash@ualberta.ca>
2016-11-09 01:15:08 -0700

    basic of html in place

--- public/orphans.js
+++ public/orphans.js
@@ -1,28 +1,74 @@
-"use strict"
+"use strict";
 
 const ORPHAN_ID_PREFIX = "orphan_";
+const crying_path_t = "../private/orphanage/assets/crying/{0}_c.png";
+const apathetic_path_t = "../private/orphanage/assets/depressed/{0}_d.png";
 
-OrphanBlock = {
-    var id,
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
 
-    init: function(id) {
-	this.id = id;
-    },
+function createMouseOutListener(id) {
+    return function() {
+	var block = document.getElementById(id);
+	console.log(crying_path_t.format(id));
+	block.style.backgroundImage = "url('" + apathetic_path_t.format(id) + "')";
+    }
+}
+
+function createMouseOverListener(id) {
+    return function() {
+	var block = document.getElementById(id);
+	console.log(crying_path_t.format(id));
+	block.style.backgroundImage = "url('" + crying_path_t.format(id) + "')";
+    }
+}
+
+function orphan(id) {
+    this.id = id;
+
+    this.getOrphanId = function() {
+	return ORPHAN_ID_PREFIX + this.id;
+    };
     
-    getOrphanBlock: function() {
-	if (id == undefined) {
-	    throw("initialize first!");
-	}
+    this.getOrphanBlock = function() {
 	var block = document.createElement("div");
-	block.id = ORPHAN_ID_PREFIX + id;
-    },
-};
+	var oID = this.getOrphanId();
+	block.id = oID;
+	block.className = "orphan";
+	console.log(apathetic_path_t.format(oID));
+	block.style.backgroundImage = "url('" + apathetic_path_t.format(oID) + "')";
+	block.addEventListener("mouseover", createMouseOverListener(oID));
+	block.addEventListener("mouseout", createMouseOutListener(oID));
+	return block;
+    };
+}
 
-OrphanContainer = {
-    orphan_blocks
-    
-    init: function() {
-	var orphan_container = document.getElementById("orphan_container");
-	
-    },
-};
+function orphanContainer() {
+    this.getOrphanContainer = function() {
+	var orphan_container = document.createElement("div");
+	orphan_container.className = "orphan_container";
+
+	var oindex;
+	for (oindex = 1; oindex <= 42; ++oindex) {
+	    var orphobj = new orphan(oindex);
+	    orphan_container.appendChild(orphobj.getOrphanBlock());
+	}
+
+	return orphan_container;
+    };
+}
+
+
+(function() {
+    var container = new orphanContainer();
+    document.body.appendChild(container.getOrphanContainer());
+})();
