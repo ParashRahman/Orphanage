64f1f7581d59afcf019f7bb596c9e684984d72e8
ParashRahman <parash@ualberta.ca>
2016-11-25 02:15:40 -0700

    refactored directory structure

--- public/initialize.js
+++ public/initialize.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const ORPHAN_ID_PREFIX = "orphan_";
-const crying_path_t = "../private/orphanage/assets/crying/{0}_c.png";
-const apathetic_path_t = "../private/orphanage/assets/depressed/{0}_d.png";
+const crying_path_t = "assets/orphanage/assets/crying/{0}_c.png";
+const apathetic_path_t = "assets/orphanage/assets/depressed/{0}_d.png";
 
 const B_CARTID = "shoppingCartButton";
 const B_CHECKOUTID = "checkOutButton";
